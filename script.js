b5be84df9006b606b01dc28e405477f818fff121
Asap <tip.no@mail.ru>
2022-06-16 21:55:30 +0300

    commit rollover

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
 //Интерактив для превьюшки.
 const start = document.querySelector(".start"),
-      startClick = start.querySelector(".start__click");
+      startClick = start.querySelector(".start__click"),
+      audio = document.querySelector(".audio");
 
 startClick.addEventListener ('click', () => {
+    audio.innerHTML = "<audio src='audio/Hans-Hujans.mp3' autoplay></audio>";
     start.style.marginTop = "-100vh";
     document.body.style.overflowY = "visible";
     document.body.style.overflowX = "hidden";
@@ -32,9 +34,9 @@ sliderContent.style.gridTemplateColumns = 'repeat(10, auto)';
 //Функция сдвига слайдера
 function dragSlider(i,callback) {
     console.log(index);
-    sliderContent.style.transition = "1s";
-    sliderContent.style.marginLeft = -i * 56 + "vw";
     callback(i);
+    sliderContent.style.transition = "0.5s";
+    sliderContent.style.marginLeft = -i * 56 + "vw";
 }
 //Функция условия index
 function conditionSlider (i) {
@@ -58,18 +60,17 @@ function conditionSlider (i) {
 function eventSlider () {
     //При клике на правую кнопку
     arrowLeft.addEventListener('click', () => {
-        index--;
+        (index > 0) ? index-- : false;
         dragSlider(index,conditionSlider);
     });
     //При клике на левую кнопку
     arrowRight.addEventListener('click', () => {
-        index++;
+        (index < 9) ? index++ : false;
         dragSlider(index,conditionSlider);
     });
 }
 //Функция события когда доходим до конца
 eventSlider();
-});
 
 //Функция для событий клика планет
 let sliderItems = document.querySelectorAll(".slider__item"),
@@ -113,3 +114,36 @@ sliderItems.forEach(function(item, i) {
             behavior:"smooth"});
     });
 });
+//2 задание лабы
+
+let planetRollover = document.querySelectorAll(".planet-rollover"),
+    planetSolarInfH2 = document.querySelector("#planets-solar__h2"),
+    planetSolarInfP = document.querySelector("#planets-solar__p"),
+    planetWrapperH2 = document.querySelectorAll('.planet__heading'),
+    planetWrapperP = document.querySelectorAll('.planet__p');
+
+
+  
+
+    //При клике
+    planetRollover.forEach(function(item,i) {
+       item.addEventListener('click', (event) => {
+            planetRollover.forEach(function(item,i) {
+                item.style.backgroundColor = "";
+                planetRollover[i].style.boxShadow = "";
+            });
+            planetRollover[i].style.backgroundColor = "rgba(0, 0, 0, 0.836)";
+            planetRollover[i].style.boxShadow = "0 0 1vw 1vw black";
+
+            if(i == 0) {
+                planetSolarInfH2.textContent = "Солнце";
+                planetSolarInfP.textContent = "Солнце — ближайшая к Земле звезда. Средняя удалённость Солнца от Земли — 149,6 млн км — приблизительно равна астрономической единице, а видимый угловой диаметр при наблюдении с Земли, как и у Луны, — чуть больше полуградуса (31—32 минуты). Солнце находится на расстоянии около 26 000 световых лет от центра Млечного Пути и вращается вокруг него на ящичной орбите, делая один оборот за 225—250 миллионов лет. Орбитальная скорость Солнца равна 217 км/с — таким образом, световой год оно проходит примерно за 1400 земных лет, а одну астрономическую единицу — за 8 земных суток.";
+            } else {
+                console.log(planetWrapperP[1]);
+                planetSolarInfH2.textContent = planetWrapperH2[i-1].textContent;
+                planetSolarInfP.textContent = planetWrapperP[i-1].textContent;
+            }
+       });
+    });
+});
+
